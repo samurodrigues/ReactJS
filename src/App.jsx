709f084084751b771d5e3ea741e1a378bf8a9e4a
Samuel S Rodrigues <samuelstievenrodrigues@gmail.com>
2023-02-17 10:49:58 -0300

    create component Formulario and ListaSuspensa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Banner from './components/Banner/Banner';
+import Formulario from './components/Formulario/Formulario';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <Formulario />
     </div>
   );
 }
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,36 @@
+import CampoTexto from '../CampoTexto/CampoTexto';
+import ListaSuspensa from '../ListaSuspensa/ListaSuspensa';
+import './Formulario.css';
+
+const Formulario = () => {
+
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+
+    return (
+        <section className='formulario'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <CampoTexto 
+                    label="Nome"
+                    placeholder="Digite seu nome"/>
+                <CampoTexto
+                    label="Cargo"
+                    placeholder="Digite seu cargo"/>
+                <CampoTexto 
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"/>
+                <ListaSuspensa label="Time" itens={times}/>
+            </form>
+        </section>
+    )
+}
+
+export default Formulario;
